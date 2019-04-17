3ee08958927b12fe0336d8f6ca8ace1c094ad394
Mike Anderson <mandersonjr19@gmail.com>
2019-04-16 22:36:22 -0600

    add bite 5

---
+++ bites_of_py/beginner/5_parse_list_of_names.py
@@ -0,0 +1,39 @@
+NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
+         'julian sequeira', 'sandra bullock', 'keanu reeves',
+         'julbob pybites', 'bob belderbos', 'julian sequeira',
+         'al pacino', 'brad pitt', 'matt damon', 'brad pitt']
+
+
+def dedup_and_title_case_names(names):
+    """Should return a list of names, each name appears only once"""
+    dedup = set(names)
+    new_list =[]
+
+    for name in dedup:
+        new_list.append(name.title())
+
+
+    return new_list
+
+
+def sort_by_surname_desc(names):
+    """Returns names list sorted desc by surname"""
+    names = dedup_and_title_case_names(names)
+    # ...
+    desc_order = sorted(names, reverse=True, key=lambda x: x.split(" ")[-1])
+
+    return desc_order
+
+def shortest_first_name(names):
+    """Returns the shortest first name (str).
+       You can assume there is only one shortest name.
+    """
+    names = dedup_and_title_case_names(names)
+    # ...
+    first_names = []
+    for name in names:
+        split_name = name.split(' ')
+        first_names.append(split_name[0])
+
+    return min(first_names, key=len)
+
